c68634d562a287d4bf16e79c07969b554c57c613
collette-tamez <cttmz4@gmail.com>
2018-11-07 16:44:53 -0800

    displayed user's guess to dom

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<div id="display"></div>
+<div id="display">
+<?php
+    if(!empty($_GET['user_guess'])){
+        print_r("your guess was ".$_GET['user_guess']); 
+    }
+?>
+</div>
 <form method="get">
     <input type="text" name="user_guess"/>
     <button>Make a guess!</button>
