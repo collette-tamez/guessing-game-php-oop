af9212ec16b608c14fcac67c1c44c5fbf9b21e15
collette-tamez <cttmz4@gmail.com>
2018-11-07 17:02:54 -0800

    checked user guess against randomly generated number

--- index.php
+++ index.php
@@ -2,14 +2,34 @@
     session_start();
     if(empty($_SESSION['random_number'])){
         $_SESSION['random_number'] = rand(1,10);
+        echo "making one";
     }
 ?>
-<div id="display">
 <?php
     if(!empty($_GET['user_guess'])){
         print_r("your guess was ".$_GET['user_guess']); 
+        if($_GET['user_guess'] > $_SESSION['random_number']){
+            $display_message = "Too hot!";
+        } else if($_GET['user_guess'] < $_SESSION['random_number']) {
+            $display_message = "Too cold!!";
+        } else if($_GET['user_guess'] == $_SESSION['random_number']) {
+            $display_message = "YA DID IT";
+        } else {
+            $display_message = "how could this have happened.....";
+        }
     }
 ?>
+<div id="display">
+   <?php 
+    if(!empty($_GET['user_guess'])){
+    ?>
+       you guessed: <?= $_GET['user_guess'] ?>
+    <?php
+        print_r($display_message);
+    } else{
+        echo "Between 1 and 10";
+    }
+    ?>
 </div>
 <form method="get">
     <input type="text" name="user_guess"/>
