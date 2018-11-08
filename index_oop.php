31a405082a00c83b591ba7fd22a47c37b6ef9b63
collette-tamez <cttmz4@gmail.com>
2018-11-07 21:24:30 -0800

    convereted procedural to object oriented

--- index_oop.php
+++ index_oop.php
@@ -1,33 +1,79 @@
 <?php
     session_start();
-    if(empty($_SESSION['random_number'])){
-        $_SESSION['random_number'] = rand(1,10);
-        echo "making one";
-        $_SESSION['history'] = [];
-    }
-?>
-<?php
-    $display_message = "Between 1 and 10";
-    if(!empty($_GET['user_guess'])){
-        $user_guess = (int)$_GET['user_guess'];
-        $_SESSION['history'][] = $user_guess;
-        print_r("your guess was: ".$_GET['user_guess']); 
-        if($user_guess > $_SESSION['random_number']){
-            $display_message = "Too hot!";
-        } else if($user_guess < $_SESSION['random_number']) {
-            $display_message = "Too cold!!";
-        } else if($user_guess == $_SESSION['random_number']) {
-            $display_message = "YA DID IT";
+    class GuessingGame{
+        private $random_number = null; //private because nothing outside of the class needs to access it
+        private $current_message = 'Make a guess!';
+        private function chose_random_number(){
+            $_SESSION['random_number'] = rand(1,10);
+        }
+        private function set_message($message){
+            $this->current_message = "<p>$message</p>";
+        }
+        private function get_number_from_session(){
+            if(empty($_SESSION['random_number'])){
+                return false;
+            } else {
+                return $_SESSION['random_number'];
+            }
+        }
+        private function get_user_guess(){
+            return (int)$_GET['user_guess'];
+        }
+        public function get_current_message(){
+            return $this->current_message;
+        }
+        private function add_history($user_guess){
+            if(empty($_SESSION['history'])){
+                $_SESSION['history'] = [];
+            }
+            $_SESSION['history'][] = $user_guess;
+        }
+        public function get_history(){
+            if(empty($_SESSION['history'])){
+               return $this->set_message("Between 1 and 10");
+            }
+            $output = '';
+            foreach($_SESSION['history'] as $number){
+                $output.="<li>$number</li>";
+            }
+            return $output;
+        }
+        public function reset_game(){
             unset($_SESSION['random_number']);
             unset($_SESSION['history']);
-        } else {
-            $display_message = "how could this have happened.....";
         }
+        public function handle_user_guess(){
+            $guess = $this->get_user_guess();
+            // $this->set_message("Your guess was: $guess");
+            // $this->get_current_message();
+            $this->add_history($guess);
+            $number_to_guess = $this->get_number_from_session();
+            if($guess > $number_to_guess){
+                $this->set_message("Too hot!");
+            } else if($guess < $number_to_guess) {
+                $this->set_message("Too cold!!");
+            } else if($guess == $number_to_guess) {
+                $this->set_message("YA DID IT"); //now we calling our method 
+                $this->reset_game();
+            } else {
+                $this->set_message("how could this have happened.....");
+            }
+        }
+        public function init(){
+            if(!$this->get_number_from_session()){
+                $this->chose_random_number();
+            }
+        }
+    }
+
+    $game = new GuessingGame;
+    $game->init();
+    if(!empty($_GET['user_guess'])){
+        $game->handle_user_guess();
     }
 ?>
 <div id="display">
-   <?= $display_message ?>
-
+   <?= $game->get_current_message(); ?>
 </div>
 <form method="get">
     <input type="text" name="user_guess"/>
@@ -36,13 +82,6 @@
 <div>
     So far you have guessed the numbers:
     <ul>
-    <?php
-        if(!empty($_SESSION['history'])){
-            foreach($_SESSION['history'] as $number){
-                ?> <li><?= $number; ?></li>
-                <?php
-            }
-        }
-    ?>
+    <?= $game->get_history(); ?>
     </ul>
 </div>
