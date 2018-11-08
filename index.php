693e0ee2d2c2941fdd9980fc600116c88ea3973b
collette-tamez <cttmz4@gmail.com>
2018-11-07 18:56:30 -0800

    added user guess history to session storage

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
     if(empty($_SESSION['random_number'])){
         $_SESSION['random_number'] = rand(1,10);
         echo "making one";
+        $_SESSION['history'] = [];
     }
 ?>
 <?php
     $display_message = "Between 1 and 10";
     if(!empty($_GET['user_guess'])){
         $user_guess = (int)$_GET['user_guess'];
-        print_r("your guess was ".$_GET['user_guess']); 
+        $_SESSION['history'][] = $user_guess;
+        print_r("your guess was: ".$_GET['user_guess']); 
         if($user_guess > $_SESSION['random_number']){
             $display_message = "Too hot!";
         } else if($user_guess < $_SESSION['random_number']) {
@@ -17,6 +19,7 @@
         } else if($user_guess == $_SESSION['random_number']) {
             $display_message = "YA DID IT";
             unset($_SESSION['random_number']);
+            unset($_SESSION['history']);
         } else {
             $display_message = "how could this have happened.....";
         }
@@ -24,6 +27,7 @@
 ?>
 <div id="display">
    <?= $display_message ?>
+
 </div>
 <form method="get">
     <input type="text" name="user_guess"/>
