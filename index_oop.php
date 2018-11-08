d506fc0028acc43aa64b69d991ae36a985882af8
collette-tamez <cttmz4@gmail.com>
2018-11-07 20:26:44 -0800

    added starting code

---
+++ index_oop.php
@@ -0,0 +1,48 @@
+<?php
+    session_start();
+    if(empty($_SESSION['random_number'])){
+        $_SESSION['random_number'] = rand(1,10);
+        echo "making one";
+        $_SESSION['history'] = [];
+    }
+?>
+<?php
+    $display_message = "Between 1 and 10";
+    if(!empty($_GET['user_guess'])){
+        $user_guess = (int)$_GET['user_guess'];
+        $_SESSION['history'][] = $user_guess;
+        print_r("your guess was: ".$_GET['user_guess']); 
+        if($user_guess > $_SESSION['random_number']){
+            $display_message = "Too hot!";
+        } else if($user_guess < $_SESSION['random_number']) {
+            $display_message = "Too cold!!";
+        } else if($user_guess == $_SESSION['random_number']) {
+            $display_message = "YA DID IT";
+            unset($_SESSION['random_number']);
+            unset($_SESSION['history']);
+        } else {
+            $display_message = "how could this have happened.....";
+        }
+    }
+?>
+<div id="display">
+   <?= $display_message ?>
+
+</div>
+<form method="get">
+    <input type="text" name="user_guess"/>
+    <button>Make a guess!</button>
+</form>
+<div>
+    So far you have guessed the numbers:
+    <ul>
+    <?php
+        if(!empty($_SESSION['history'])){
+            foreach($_SESSION['history'] as $number){
+                ?> <li><?= $number; ?></li>
+                <?php
+            }
+        }
+    ?>
+    </ul>
+</div>
